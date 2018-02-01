90a471ade8ab6e41943ff1ab1eaf070b4547bb0b
Petteri Piironen <pettepi@ety-a2123b-14.edu.wks.metropolia.fi>
2018-02-01 13:01:00 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
+import {SetupComponent} from './setup/setup.component';
+import {RoutesComponent} from './routes/routes.component';
+import {HttpClientModule} from '@angular/common/http';
+import {DigitransitService} from './services/digitransit.service';
+import {FormsModule} from '@angular/forms';
 
+import {AgmCoreModule} from '@agm/core';
+import { RouteDirectionPipe } from './pipes/route-direction.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    SetupComponent,
+    RoutesComponent,
+    RouteDirectionPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD7jJf_I4WQrraXdAvPZXUxvqMm6qzbpwc',
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DigitransitService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
+
+
